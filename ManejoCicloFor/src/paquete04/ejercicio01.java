bc3b05be491849896bed90ecc2f16e965d9968a6
CarlosM1103 <carlosmedinal2006@gmail.com>
2024-12-05 11:38:15 -0500

    Hechooo

---
+++ ManejoCicloFor/src/paquete04/ejercicio01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete04;
+
+/**
+ *
+ * @author utpl
+ */
+public class ejercicio01 {
+
+    public static void main(String[] args) {
+
+        for (int num = 30; num >= 10; num--) {
+            System.out.println(num + "-" + num * 2 + "-" + num * 3 + "-" + num * 4);
+        }
+
+    }
+
+}
